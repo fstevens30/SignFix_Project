15c91cdc08ccc4b8fbd71c1a591f80a91e7663d8
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-05-31 13:16:51 +1200

    Added channelAmount function

--- SignFix  Project/Program.cs
+++ SignFix  Project/Program.cs
@@ -21,7 +21,24 @@ namespace Calculator
                 channelType = "small channel";
             };
             int channelLength = signLength - 100;
-            int channelAmount = signWidth % 100;
+            int channelAmount = 0;
+            if (signWidth >= 1000)
+            {
+                channelAmount = 4;
+            }
+            else if (signWidth <= 1000 & signWidth > 400)
+            {
+                channelAmount = 3;
+            }
+            else if (signWidth <= 400 & signWidth > 100)
+            {
+                channelAmount = 2;
+            }
+            else if (signWidth <= 100 & signWidth > 0)
+            {
+                channelAmount = 1;
+            }
+            else Console.WriteLine("Error: Invalid Width");
             Console.WriteLine("Sign requires " + channelAmount + "x length(s) of " + channelType + " at, " + channelLength + "mm long.");
         }
     }
